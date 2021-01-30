067d74a20e4c6420c006a9505c9f290cb729915d
TERY <fire_jacky@mail.ru>
2021-01-30 16:28:34 +0300

    add modal window

--- js/script.js
+++ js/script.js
@@ -96,19 +96,22 @@ window.addEventListener('DOMContentLoaded', () => {
          modal         = document.querySelector('.modal'),
          modalCloseBtn = document.querySelector('[data-close]');
 
-   modalTrigger.forEach(btn => {
-      btn.addEventListener('click', () => {
-         modal.classList.add('show');
-         modal.classList.remove('hide');
-         document.body.style.overflow = 'hidden';
-      });
-   });
+   function openModal() {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);
+   }
 
    function closeModal() {
-      modal.classList.add('hide');
-      modal.classList.remove('show');
-      document.body.style.overflow = '';
+         modal.classList.add('hide');
+         modal.classList.remove('show');
+         document.body.style.overflow = '';
    }
+
+   modalTrigger.forEach(btn => {
+      btn.addEventListener('click', openModal);
+   });
 
    modalCloseBtn.addEventListener('click', closeModal);
 
@@ -123,4 +126,18 @@ window.addEventListener('DOMContentLoaded', () => {
          closeModal();
       }
    });
+
+   const modalTimerId = setTimeout(openModal, 5000);
+
+   function showModalByScroll() {
+      if (window.pageYOffset + 
+         document.documentElement.clientHeight >= 
+         document.documentElement.scrollHeight) 
+      {
+         openModal();
+         window.removeEventListener('scroll', showModalByScroll);
+      }
+   }
+
+   window.addEventListener('scroll', showModalByScroll);
 });
